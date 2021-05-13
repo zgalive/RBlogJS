6d26fc3f5c1d2956d051c43deca9f1acf9b68046
GangZhu <gang@GangZhus-MacBook-Pro.local>
2021-05-13 16:14:31 +0800

    fix

--- webpack.github.js
+++ webpack.github.js
@@ -8,7 +8,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    publicPath: '/RBlogJS',
+    publicPath: '/RBlogJS/',
     filename: '[name].[hash].js',
     path: path.resolve('dist')
   },
